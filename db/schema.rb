7c6e119dadf3d689b044acdd7bc7e5cb2f3416be
sk918 <skmtmt96918@gmail.com>
2021-03-02 19:59:35 +0900

    DBのUsersテーブルのカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_03_02_044530) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name", null: false
