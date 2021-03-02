175b8bb1cff3e1363d8dcb85aec844e37f4ab960
sk918 <skmtmt96918@gmail.com>
2021-03-02 16:28:22 +0900

    Userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ZENKAKU_NAME = /\A[ぁ-んァ-ン一-龥]/
+ZENKAKU_NAME_KANA = /\A[ァ-ヶー－]+\z/
+
+validates :nickname, presence: true
+validates :family_name, presence: true, format: { with: ZENKAKU_NAME }
+validates :first_name, presence: true, format: { with: ZENKAKU_NAME }
+validates :family_name_kana, presence: true, format: { with: ZENKAKU_NAME_KANA }
+validates :first_name_kana, presence: true, format: { with: ZENKAKU_NAME_KANA }
+validates :birth_date, presence: true
+
 end
