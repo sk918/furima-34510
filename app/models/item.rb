9ed120ed01cf574722791f936f7b41b02abb9bcd
sk918 <skmtmt96918@gmail.com>
2021-03-03 21:38:37 +0900

    itemモデルバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,8 @@
 class Item < ApplicationRecord
 
-  validates :price, format { with: /\A[0-9]+\z/ }, 
-                             allow_blank: true,
-                             numericality: { only_integer: true, greater_than: 299, less_than: 10,000,000}
-  validates :title, length { maximum: 40}
-  validates :description, length { maximum: 1000}
+  validates :price, format: { with: /\A[0-9]+\z/ }, allow_blank: true, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 } 
+  validates :title, length: { maximum: 40}
+  validates :description, length: { maximum: 1000}
 
   with_options presence: true do
     validates :image
