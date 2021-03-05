4088639f48f7a5275270f752f0a102ad9d9965ad
sk918 <skmtmt96918@gmail.com>
2021-03-05 23:32:04 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    Item.find(params[:id]).destroy
+    redirect_to items_path
+  end
+
   private
 
   def item_params
