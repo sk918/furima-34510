8c22957db216c3a9a5c7d98f20550a0f6f5cb651
sk918 <skmtmt96918@gmail.com>
2021-03-06 14:54:33 +0900

    商品削除機能：レビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -35,8 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    Item.find(params[:id]).destroy
-    redirect_to items_path
+    @item.destroy
+    redirect_to root_path
   end
 
   private
